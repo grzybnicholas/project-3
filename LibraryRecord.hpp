36da1b6ae74a25ff1fae8f6931876e43b013bdf8
grzybnicholas <92946140+grzybnicholas@users.noreply.github.com>
2023-03-09 02:02:05 +0000

    included cpp

--- LibraryRecord.hpp
+++ LibraryRecord.hpp
@@ -21,5 +21,5 @@ class LibraryRecord : public ArrayBag<Book>{
  void operator+=(const LibraryRecord& recs);
  void operator/=(const LibraryRecord& record);
 };
-
+#include "LibraryRecord.cpp"
 #endif
